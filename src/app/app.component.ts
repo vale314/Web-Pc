170808aae4cb2d03c184da0ad2ec4550713ec570
vale314 <electrovale314@hotmail.com>
2018-08-11 15:53:22 -0500

    1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '../../node_modules/@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private image:boolean = true;
+  private logoB:boolean = true;
+  constructor(private location:Location){
+   }
+   
+  ngOnInit(){
+    this.logoH();
+    this.imgW();
+  }
+
+  logoH(){
+    if (window.outerHeight > 630) {
+      return this.logoB = true;
+    }else{
+      return this.logoB = false;
+    }
+  }
+  
+
+  imgW(){
+    if (window.outerWidth > 768) {
+      return this.image = true;
+    }else{
+      return this.image = false;
+
+    }
+  }
+
+  
+
+  
+  isHome() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+
+    if( titlee === '/home' ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+  isDocumentation() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if( titlee === '/documentation' ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 }
